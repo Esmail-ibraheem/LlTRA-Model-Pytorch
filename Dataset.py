adcf144f43192ecc9aa576df4fee01b0d3c853b5
Esmail A.Gumaan <113830751+Esmail-ibraheem@users.noreply.github.com>
2024-01-04 13:53:27 +0300

    Add files via upload

--- Dataset.py
+++ Dataset.py
@@ -12,12 +12,12 @@ class BilingualDataset(Dataset):
         self.target_language = target_language
         self.sequence_length = sequence_length
 
-        #Special Tokens 
+        #special tokens 
         self.SOS_token = torch.Tensor(source_tokenizer.token_to_id("[SOS]"), dtype = torch.int64)
         self.PAD_token = torch.Tensor(source_tokenizer.token_to_id("[PAD]"), dtype = torch.int64)
         self.EOS_token = torch.Tensor(source_tokenizer.token_to_id("[EOS]"), dtype = torch.int64)
 
-    def __len__(self):
+    def __length__(self):
         return len(self.dataset)
 
     def __getitem__(self, index) -> any:
@@ -25,11 +25,11 @@ class BilingualDataset(Dataset):
         source_text = source_target_dataset['translation'][self.source_language]
         target_text = source_target_dataset['translation'][self.target_language]
 
-        encode_source_tokenizer = self.source_tokenizer.encode(source_text, dtype = torch.int64).ids 
-        encode_target_tokenizer = self.target_tokenizer.encode(target_text, dtype = torch.int64).ids 
+        encode_source_tokenizers = self.source_tokenizer.encode(source_text).ids 
+        encode_target_tokenizers = self.target_tokenizer.encode(target_text).ids 
 
-        encode_source_padding = self.sequence_length - len(encode_source_tokenizer) - 2
-        encode_target_padding = self.sequence_length - len(encode_target_tokenizer) -1
+        encode_source_padding = self.sequence_length - len(encode_source_tokenizers) - 2
+        encode_target_padding = self.sequence_length - len(encode_target_tokenizers) - 1
 
         if encode_source_padding < 0 or encode_target_padding < 0:
             raise ValueError("the sequence is too long")
@@ -37,42 +37,44 @@ class BilingualDataset(Dataset):
         encoder_source = torch.cat(
             [
                 self.SOS_token,
-                torch.tensor(encode_source_tokenizer, dtype= torch.int64),
+                torch.tensor(encode_source_tokenizers, dtype=torch.int64),
                 self.EOS_token,
                 torch.tensor([self.PAD_token] * encode_source_padding, dtype=torch.int64)
             ]
-        ) 
+        )
 
         encoder_target = torch.cat(
             [
                 self.SOS_token,
-                torch.tensor(encode_target_tokenizer, dtype= torch.int64),
-                torch.tensor([self.PAD_token] * encode_target_padding, dtype=torch.int64)
+                torch.tensor(encode_target_tokenizers, dtype= torch.int64),
+                torch.tensor([self.PAD_token] * encode_target_padding, dtype= torch.int64)
             ]
         )
 
         Target = torch.cat(
             [
-                torch.tensor(encode_target_tokenizer, dtype= torch.int64),
-                torch.tensor([self.PAD_token] * encode_target_padding, dtype=torch.int64),
-                self.EOS_token
+                torch.tensor(encode_target_tokenizers, dtype=torch.int64),
+                self.EOS_token,
+                torch.tensor([self.PAD_token] * encode_target_padding, dtype= torch.int64)
             ]
         )
 
         assert encoder_source.size(0) == self.sequence_length 
         assert encoder_target.size(0) == self.sequence_length 
-        assert Target(0) == self.sequence_length 
+        assert Target.size(0) == self.sequence_length 
 
         return {
             "encoder_source": encoder_source,
             "encoder_target": encoder_target,
-            "encoder_source_mask": (encoder_source != self.PAD_token).unsqueeze(0).unsqueeze(0).int(),
-            "encoder_target_mask": (encoder_target != self.PAD_token).unsqueeze(0).unsqueeze(0).int() & casual_mask(encoder_target.size()),
+            "encoder_source_mask" : (encoder_source != self.PAD_token).unsqueeze(0).unsqueeze(0).int(),
+            "encoder_target_mask" : (encoder_target != self.PAD_token).unsqueeze(0).unsqueeze(0).int() & casual_mask(encoder_target.size()),
             "Target": Target,
             "source_text": source_text,
             "target_text": target_text
         }
 
+
 def casual_mask(size):
-    mask = torch.triu(torch.ones(1, size, size), diagonal= 1).dtype(torch.int64)
-    return mask == 0 
+    mask = torch.triu(torch.ones(1, size, size), diagonal=1).dtype(torch.int64)
+    return mask == 0
+    
--- train.py
+++ train.py
@@ -1,42 +1,70 @@
-from importlib.resources import path
-import torch 
-import torch.nn as nn
-from torch.utils.data import DataLoader, Dataset, random_split
+import torch
+import torch.nn as nn 
+from torch.utils.data import Dataset, DataLoader, random_split
+
+from Dataset import BilingualDataset, casual_mask
 
 from datasets import load_dataset
+
+from model import TransformerModel
 
 from tokenizers import Tokenizer
 from tokenizers.models import WordLevel
 from tokenizers.pre_tokenizers import Whitespace
 from tokenizers.trainers import WordLevelTrainer
 
-from pathlib import Path
+from pathlib import Path 
 
-def Get_All_Sentences(dataset, language):
+def Get_All_sentences(dataset, language):
     for lang in dataset:
         yield lang['translation'][language]
 
-def Build_Tokenizer(configuration, dataset, language):
-    tokenizer_path = Path(configuration["tokenizer_file"].format(language))
+def Build_Tokenizers(configuration, dataset, language):
+    tokenizer_path = Path(configuration['tokenizer_file'].format(language))
     if not Path.exists(tokenizer_path):
         tokenizer = Tokenizer(WordLevel(unk_token="UNK"))
         tokenizer.pre_tokenizer = Whitespace()
-        trainer = WordLevelTrainer(special_tokens = ["UNK", "SOS", "EOS"], min_frequency = 2)
-        tokenizer.train_from_iterator(Get_All_Sentences(dataset, language) ,trainer = trainer)
+        trainer = WordLevelTrainer(specail_tokens = ["UNK", "PAD", "SOS", "EOS"], min_frequency = 2)
+        tokenizer.train_from_iterator(Get_All_sentences(dataset, language), trainer=trainer)
         tokenizer.save(str(tokenizer_path))
 
     else:
         tokenizer = Tokenizer.from_file(str(tokenizer_path))
 
-def Load_Dataset(configuration, dataset, language):
-    dataset_Raw = load_dataset("ImruQays/Thaqalayn-Classical-Arabic-English-Parallel-texts", f"{configuration['source_language']} - {configuration['target_language']}", split="train")
 
-    source_dataset_Raw = Build_Tokenizer(configuration, dataset_Raw, configuration['source_language'])
-    target_dataset_Raw = Build_Tokenizer(configuration, dataset_Raw, configuration['target_language'])
+def Get_dataset(configuration):
+    dataset_Raw = load_dataset("", f"{configuration['source_language']} - {configuration['target_language']}", split="train")
+    
+    source_tokenizer = Build_Tokenizers(configuration, dataset_Raw, configuration['source_language'])
+    target_tokenizer = Build_Tokenizers(configuration, dataset_Raw, configuration['target_language'])
 
     train_dataset_size = (0.9 * len(dataset_Raw))
-    validation_dataset_size = (len(dataset_Raw) - train_dataset_size)
+    validation_dataset_size = len(dataset_Raw) - train_dataset_size
 
     train_dataset_Raw, validation_dataset_Raw = random_split(dataset_Raw, [train_dataset_size, validation_dataset_size])
 
+    train_dataset = BilingualDataset(train_dataset_Raw, source_tokenizer, target_tokenizer, configuration['soruce_language'], configuration['target_language'], configuration['sequence_length'])
+    validation_dataset = BilingualDataset(validation_dataset_Raw, source_tokenizer, target_tokenizer, configuration['soruce_language'], configuration['target_language'], configuration['sequence_length'])
+
+    maximum_source_sequence_length = 0
+    maximum_target_sequence_length = 0
+
+    for item in dataset_Raw:
+        source_ids = source_tokenizer.encode(item['translation']['source_language']).ids
+        target_ids = target_tokenizer.encode(item['translation']['target_language']).ids
+        maximum_source_sequence_length = max(maximum_source_sequence_length, source_ids)
+        maximum_target_sequence_length = max(maximum_target_sequence_length, target_ids)
+
+    print(f"maximum_source_sequence_length: {maximum_source_sequence_length}")
+    print(f"maximum_target_sequence_length: {maximum_target_sequence_length}")
+
+    train_dataLoader = DataLoader(train_dataset, batch_size=configuration['batch_size'], shuffle= True)
+    validation_dataLoader = DataLoader(validation_dataset, batch_size=1, shuffle= True)
     
+    return train_dataLoader, validation_dataLoader, source_tokenizer, target_tokenizer
+
+def Get_model(configuration, source_vocab_length, target_vocab_length):
+    model = TransformerModel(source_vocab_length, target_vocab_length, configuration['sequence_length'], configuration['sequence_length'], configuration['d_model'])
+    return model 
+
+
