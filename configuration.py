9e331039ab50caddad7b5f11a95e961ef0ca0549
Esmail A.Gumaan <113830751+Esmail-ibraheem@users.noreply.github.com>
2024-01-04 13:55:34 +0300

    settings

---
+++ configuration.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+
+def get_config():
+    return {
+        "batch_size": 8,
+        "num_epochs": 20,
+        "lr": 10**-4,
+        "sequence_length": 350,
+        "d_model": 512,
+        "datasource": 'ImruQays/Thaqalayn-Classical-Arabic-English-Parallel-texts',
+        "source_language": "Arabic",
+        "target_language": "English",
+        "model_folder": "weights",
+        "model_basename": "tmodel_",
+        "preload": "latest",
+        "tokenizer_file": "tokenizer_{0}.json",
+        "experiment_name": "runs/tmodel"
+    }
+
+def get_weights_file_path(config, epoch: str):
+    model_folder = f"{config['datasource']}_{config['model_folder']}"
+    model_filename = f"{config['model_basename']}{epoch}.pt"
+    return str(Path('.') / model_folder / model_filename)
