6ac78b1d248d509e45ec85469349dfa1eef4ab1e
Esmail A.Gumaan <113830751+Esmail-ibraheem@users.noreply.github.com>
2024-01-08 11:16:15 +0300

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -8,8 +8,8 @@ def Get_configuration():
         "sequence_length": 350,
         "d_model": 512,
         "datasource": 'opus_books',
-        "source_lenguage": "en",
-        "lang_tgt": "it",
+        "source_language": "en",
+        "target_language": "it",
         "model_folder": "weights",
         "model_basename": "tmodel_",
         "preload": "latest",
@@ -21,3 +21,12 @@ def Get_weights_file_path(config, epoch: str):
     model_folder = f"{config['datasource']}_{config['model_folder']}"
     model_filename = f"{config['model_basename']}{epoch}.pt"
     return str(Path('.') / model_folder / model_filename)
+
+def latest_weights_file_path(config):
+    model_folder = f"{config['datasource']}_{config['model_folder']}"
+    model_filename = f"{config['model_basename']}*"
+    weights_files = list(Path(model_folder).glob(model_filename))
+    if len(weights_files) == 0:
+        return None
+    weights_files.sort()
+    return str(weights_files[-1])
