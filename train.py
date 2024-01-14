28ece7cdfaddcc1c9ea70387cc158e56cbce4655
Esmail A.Gumaan <113830751+Esmail-ibraheem@users.noreply.github.com>
2024-01-14 18:20:59 +0300

    Update train.py

--- train.py
+++ train.py
@@ -20,60 +20,60 @@ import warnings
 from tqdm import tqdm
 import os 
 
-def greedy_search(model, source, source_mask, source_tokenizer, target_tokenizer, max_len, device):
-    sos_idx = target_tokenizer.token_to_id('[SOS]')
-    eos_idx = target_tokenizer.token_to_id('[EOS]')
+# def greedy_search(model, source, source_mask, source_tokenizer, target_tokenizer, max_len, device):
+#     sos_idx = target_tokenizer.token_to_id('[SOS]')
+#     eos_idx = target_tokenizer.token_to_id('[EOS]')
 
-    encoder_output = model.encode(source, source_mask)
+#     encoder_output = model.encode(source, source_mask)
     
-    decoder_input = torch.empty(1, 1).fill_(sos_idx).type_as(source).to(device)
-    while True:
-        if decoder_input.size(1) == max_len:
-            break
+#     decoder_input = torch.empty(1, 1).fill_(sos_idx).type_as(source).to(device)
+#     while True:
+#         if decoder_input.size(1) == max_len:
+#             break
 
-        decoder_mask = casual_mask(decoder_input.size(1)).type_as(source_mask).to(device)
+#         decoder_mask = casual_mask(decoder_input.size(1)).type_as(source_mask).to(device)
 
-        out = model.decode(encoder_output, source_mask, decoder_input, decoder_mask)
+#         out = model.decode(encoder_output, source_mask, decoder_input, decoder_mask)
 
-        # get next token (get the token with the maximum probabilty)
-        prob = model.linear(out[:, -1])
-        _, next_word = torch.max(prob, dim=1)
-        decoder_input = torch.cat(
-            [decoder_input, torch.empty(1, 1).type_as(source).fill_(next_word.item()).to(device)], dim=1
-        )
+#         # get next token (get the token with the maximum probabilty)
+#         prob = model.linear(out[:, -1])
+#         _, next_word = torch.max(prob, dim=1)
+#         decoder_input = torch.cat(
+#             [decoder_input, torch.empty(1, 1).type_as(source).fill_(next_word.item()).to(device)], dim=1
+#         )
 
-        if next_word == eos_idx:
-            break
+#         if next_word == eos_idx:
+#             break
 
-    return decoder_input.squeeze(0)
+#     return decoder_input.squeeze(0)
 
 
-def run_validation(model, validation_ds, source_tokenizer, target_tokenizer, max_len, device, print_msg, global_step, writer, num_examples=2):
-    model.eval()
-    count = 0
-    console_width = 80
+# def run_validation(model, validation_ds, source_tokenizer, target_tokenizer, max_len, device, print_msg, global_step, writer, num_examples=2):
+#     model.eval()
+#     count = 0
+#     console_width = 80
 
-    with torch.no_grad():
-        for batch in validation_ds:
-            count += 1
-            encoder_input = batch["encoder_input"].to(device) 
-            encoder_mask = batch["encoder_input_mask"].to(device) 
+#     with torch.no_grad():
+#         for batch in validation_ds:
+#             count += 1
+#             encoder_input = batch["encoder_input"].to(device) 
+#             encoder_mask = batch["encoder_input_mask"].to(device) 
 
-            assert encoder_input.size(0) == 1, "Batch size must be 1 for validation"
+#             assert encoder_input.size(0) == 1, "Batch size must be 1 for validation"
 
-            model_out = greedy_search(model, encoder_input, encoder_mask, source_tokenizer, target_tokenizer, max_len, device)
+#             model_out = greedy_search(model, encoder_input, encoder_mask, source_tokenizer, target_tokenizer, max_len, device)
 
-            source_text = batch["target_text"][0]
-            target_text = batch["target_text"][0]
-            model_out_text = target_tokenizer.decode(model_out.detach().cpu().numpy())
+#             source_text = batch["target_text"][0]
+#             target_text = batch["target_text"][0]
+#             model_out_text = target_tokenizer.decode(model_out.detach().cpu().numpy())
 
-            print_msg('-'*console_width)
-            print_msg(f"{f'SOURCE: ':>12}{source_text}")
-            print_msg(f"{f'TARGET: ':>12}{target_text}")
-            print_msg(f"{f'PREDICTED: ':>12}{model_out_text}")
+#             print_msg('-'*console_width)
+#             print_msg(f"{f'SOURCE: ':>12}{source_text}")
+#             print_msg(f"{f'TARGET: ':>12}{target_text}")
+#             print_msg(f"{f'PREDICTED: ':>12}{model_out_text}")
 
-            if count == num_examples:
-                break
+#             if count == num_examples:
+#                 break
 
 def Get_All_Sentences(dataset, language):
     for lang in dataset:
@@ -183,7 +183,7 @@ def train_model(configuration):
             optimizer.step()
             optimizer.zero_grad(set_to_none=True)
 
-            run_validation(model, validation_dataLoader, source_tokenizer, target_tokenizer, configuration['sequence_length'], device, lambda msg: batch_iterator.write(msg), global_step, writer)
+            # run_validation(model, validation_dataLoader, source_tokenizer, target_tokenizer, configuration['sequence_length'], device, lambda msg: batch_iterator.write(msg), global_step, writer)
 
             global_step += 1
 
